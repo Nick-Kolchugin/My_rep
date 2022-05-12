23759199bcb4dafbac73e4e538bfc56598a8d7d3
Nick-Kolchugin <drnik71@gmail.com>
2022-05-12 12:16:18 +0600

    New commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Вывод");
         System.out.println("Нововведение");
+        System.out.println("Новый вывод с коммитом");
     }
 }
